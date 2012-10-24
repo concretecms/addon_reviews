aaf4e1a1657239a86ad5dafdf033aac846d781f8
Andrew Embler <andrew@concrete5.org>
2012-10-23 17:19:16 -0700

    layout tweaks to form

--- packages/reviews/blocks/reviews/form_setup_html.php
+++ packages/reviews/blocks/reviews/form_setup_html.php
@@ -12,36 +12,40 @@
 <div class="clearfix">
 	<?php echo $form->label('requireApproval', t('Comments Require Moderator Approval?'));?>
 	<div class="input">
-	<?php echo $form->radio("requireApproval", 1, $requireApproval);?><?php echo t('Yes');?>
-	<?php echo "<br/>"?>
-	<?php echo $form->radio("requireApproval", 0, $requireApproval);?><?php echo t('No')?>
-	</div><!--class="input"-->
+	<ul class="inputs-list">
+		<li><?php echo $form->radio("requireApproval", 1, $requireApproval);?> <span><?php echo t('Yes');?></span></li>
+		<li><?php echo $form->radio("requireApproval", 0, $requireApproval);?> <span><?php echo t('No')?></span></li>
+	</ul>
+</div>
 </div><!--clearfix --> 
 
 <div class="clearfix">
 	<?php echo $form->label('displayGuestBookForm',t('Posting Reviews is Enabled?'));?>
 	<div class="input">
-	<?php echo $form->radio("displayGuestBookForm", 1, $displayGuestBookForm);?><?php echo t('Yes')?>
-	<?php echo "<br/>"?>
-	<?php echo $form->radio("displayGuestBookForm", 0, $displayGuestBookForm);?><?php echo t('No');?>
+	<ul class="inputs-list">
+		<li><?php echo $form->radio("displayGuestBookForm", 1, $displayGuestBookForm);?> <span><?php echo t('Yes')?></span></li>
+		<li><?php echo $form->radio("displayGuestBookForm", 0, $displayGuestBookForm);?> <span><?php echo t('No');?></span></li>
+	</ul>
 	</div><!--class="input"-->
 </div><!--clearfix --> 
 
 <div class="clearfix">
 	<?php echo $form->label('authenticationRequired',t('Authentication Required to Post?'));?>
 	<div class="input">
-	<?php echo $form->radio("authenticationRequired", 0, $authenticationRequired);?><?php echo t(' Email Only')?>
-	<?php echo "<br/>"?>
-	<?php echo $form->radio("authenticationRequired", 1, $authenticationRequired); ?><?php echo t(' Users must login to C5')?>
+	<ul class="inputs-list">
+		<li><?php echo $form->radio("authenticationRequired", 0, $authenticationRequired);?> <span><?php echo t('Email Only')?></span></li>
+		<li><?php echo $form->radio("authenticationRequired", 1, $authenticationRequired); ?> <span><?php echo t('Users must login to concrete5')?></span></li>
+	</ul>	
 	</div><!--class="input"-->
 </div><!--clearfix --> 
 
 <div class="clearfix">
 	<?php echo $form->label('displayCaptcha',t('Solving a <a href="%s" target="_blank">CAPTCHA</a> Required to Post?', 'http://en.wikipedia.org/wiki/Captcha'));?>
 	<div class="input">
-	<?php echo $form->radio("displayCaptcha",1, $displayCaptcha);?><?php  echo t('Yes')?>
-	<?php echo "<br/>"?>
-	<?php echo $form->radio("displayCaptcha",0, $displayCaptcha );?><?php  echo t('No')?>
+	<ul class="inputs-list">
+		<li><?php echo $form->radio("displayCaptcha",1, $displayCaptcha);?> <span><?php  echo t('Yes')?></span></li>
+		<li><?php echo $form->radio("displayCaptcha",0, $displayCaptcha );?> <span><?php  echo t('No')?></span></li>
+	</ul>
 	</div><!--class="input"-->
 </div><!--clearfix -->
  
